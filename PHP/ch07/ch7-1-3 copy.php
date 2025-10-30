280e9478ee40c7b6a976f74510e80f458bb8e941
Samuel633653 <Samuel633653@gmail,com>
2025-10-30 09:57:14 +0800

    week 8

--- PHP/ch07/ch7-1-3 copy.php
+++ PHP/ch07/ch7-1-3 copy.php
@@ -1,10 +1,16 @@
+
+SID:C112181104
+<BR>
+NAME:何昇輔
+<p>
 <?php
 $scores = array(22, 16, 30, 24);
 $total = 0;
 foreach ($scores as $element){
-   echo "$element ";
+   echo "$element |";
    $total += $element;
 }
+
 echo "<BR>$total<BR>";
 print_r(value: $scores);
 ?>
